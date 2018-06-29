36f487d1313ec5c8ca7385689f143102e8fe9a2f
Manuel Páez <m.paez589@gmail.com>
2018-06-29 12:18:33 +0200

    Add files via upload

--- src/main/java/com/example/RemoteClassLoader/MyClassLoader.java
+++ src/main/java/com/example/RemoteClassLoader/MyClassLoader.java
@@ -14,39 +14,40 @@ import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
 import org.reflections.util.FilterBuilder;
 
+import com.example.interfaces.iAnimal;
+
 public class MyClassLoader {
 
 	public void getJar() {
 
 		try {
 				
-			URL fileWeb = new URL("https://github.com/otamega93/remoteJarTest/blob/master/test.jar");
+			//URL fileWeb = new URL("https://github.com/otamega93/remoteJarTest/blob/master/test.jar");
 			//URL fileWeb = new URL("https://encodable.com/uploaddemo/files/test.jar");
 
-			//URL fileWeb = new URL("file:\\C:\\Users\\mapaez\\Desktop\\test.jar");
+			URL fileWeb = new URL("file:\\C:\\Users\\mapaez\\Desktop\\test.jar");
 			
 			URL urlWeb = fileWeb.toURI().toURL();
 			URL[] urlsWeb = new URL[] { urlWeb };
 
-			ClassLoader clWeb = new URLClassLoader(urlsWeb);
+			ClassLoader clWeb = new URLClassLoader(urlsWeb, this.getClass().getClassLoader());
 			
-			Class clsWeb = clWeb.loadClass("com.example.modules.M2");
+			//Class clsWeb = clWeb.loadClass("com.example.modules.M2");
 			
-			Method methodRegularClassLoader = clsWeb.getMethod("test");
-			methodRegularClassLoader.invoke(clsWeb.newInstance());
+			//Method methodRegularClassLoader = clsWeb.getMethod("test");
+			//methodRegularClassLoader.invoke(clsWeb.newInstance());
 
 			Configuration config = new ConfigurationBuilder()
-					.setUrls(ClasspathHelper.forPackage("com.example.modules", clWeb)).addClassLoader(clWeb)
+					.setUrls(ClasspathHelper.forClassLoader(clWeb)).addClassLoader(clWeb)
 					.filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix("com.example.modules")))
 					.setScanners(new SubTypesScanner(false), new ResourcesScanner(), new TypeAnnotationsScanner());
 
 			Reflections reflections = new Reflections(config);
 
-			Set<Class<? extends Animal>> setModulos = reflections.getSubTypesOf(Animal.class);
+			Set<Class<? extends Object>> setModulos = reflections.getSubTypesOf(Object.class);
 
-			for (Class<? extends Animal> modulo : setModulos) {
+			for (Class<? extends Object> modulo : setModulos) {
 
-				// System.out.println(counter++);
 				Method methodWeb = modulo.getMethod("test");
 				methodWeb.invoke(modulo.newInstance());
 			}
